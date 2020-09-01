7486790d8bff6608b8830b46a741504056cfebf7
Lucas Melo <lucasmelo2597@gmail.com>
2020-08-31 22:19:32 -0300

    Bot com json

---
+++ botjson.py
@@ -0,0 +1,37 @@
+#-- coding: utf-8 --
+import telebot
+import json
+import urllib.request
+
+#TOKEN DO BOT
+API_TOKEN = 'TOKEN DO BOT'
+
+bot = telebot.TeleBot(API_TOKEN)
+
+@bot.message_handler(commands=['cep'])
+def send_cep(message):
+    msg = bot.reply_to(message, """Digite o CEP que deseja consultar:""")
+    cid = message.chat.id #id da conversa
+    bot.register_next_step_handler(msg, send_cep_step) # a mensagem digitada, vai ser atribuida a variavél message_cep
+
+def send_cep_step(message):
+    cid = message.chat.id
+    mensagem_cep = message.text 
+    url = "https://viacep.com.br/ws/" + mensagem_cep + "/json/"
+    response = urllib.request.urlopen(url)
+    data = json.loads(response.ready())
+
+    cep = data['cep']
+    logradouro = data['logradouro']
+    bairro = data['bairro']
+    localidade = data['localidade']
+    uf = data['uf']
+    
+    bot.send_message(cid, "CEP: " + cep + "\nLogradouro: " + logradouro + "\nBairro: " + bairro + "\nLocalidade: " + localidade + "\nUF: " + uf)
+
+
+bot.polling()
+    
+
+
+    
